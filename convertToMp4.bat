cfe25576a3542359ca83af44a85de23996089a6b
TheDarkTron <darktron1337@gmail.com>
2020-01-02 10:34:56 +0100

    removed -tune film as this tune is no longer supported

--- convertToMp4.bat
+++ convertToMp4.bat
@@ -6,7 +6,7 @@ rem %1 is the first drag drop parameter. The "~" removes special chracters
 ECHO "%~1"
 ECHO.
 
-ffmpeg -n -hwaccel auto -i "%~1" -c:v libx265 -crf 28 -tune film -threads 7 "%~dpn1_small.mp4"
+ffmpeg -n -hwaccel auto -i "%~1" -c:v libx265 -crf 28 -threads 7 "%~dpn1_small.mp4"
 
 rem shift %3 tp %2, %2 to %1...
 SHIFT
