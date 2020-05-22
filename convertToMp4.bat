a133303a45c78b662d33c8556c13a704b5559718
TheDarkTron <darktron1337@gmail.com>
2020-05-22 12:49:34 +0200

    tags are preserved

--- convertToMp4.bat
+++ convertToMp4.bat
@@ -5,7 +5,7 @@ ECHO converting to mp4: "%~1"
 REM %1 is the first drag drop parameter. The "~" removes special chracters
 ECHO.
 
-ffmpeg -n -hwaccel auto -i "%~1" -c:v libx265 -crf 28 -tag:v hvc1 -threads 7 "%~dpn1_HEVC.mp4"
+ffmpeg -n -hwaccel auto -i "%~1" -movflags use_metadata_tags -c:v libx265 -crf 28 -tag:v hvc1 -threads 7 "%~dpn1_HEVC.mp4"
 
 REM shift to next file
 SHIFT
